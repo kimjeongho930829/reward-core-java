28a4df0f05226c666a64dcd30313190ccc4e8acb
kimjeongho930829 <kimjeongho930829@gmail.com>
2026-02-11 18:41:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/reward/core/ResilientRewardCoreApplication.java
@@ -0,0 +1,13 @@
+package com.reward.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientRewardCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResilientRewardCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reward/core/ResilientRewardCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reward.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilientRewardCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
